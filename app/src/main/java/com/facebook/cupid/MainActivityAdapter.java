9aad391b0b75bb6040210d0548ee42c144195cbe
Emily Roth <emilylroth@utexas.edu>
2017-11-11 14:55:41 -0600

    Friends list adapter class

--- app/src/main/java/com/facebook/cupid/MainActivityAdapter.java
+++ app/src/main/java/com/facebook/cupid/MainActivityAdapter.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-import static android.support.v7.recyclerview.R.styleable.RecyclerView;
 
 /**
  * Created by Emily on 11/11/2017.
@@ -21,31 +20,52 @@ import static android.support.v7.recyclerview.R.styleable.RecyclerView;
 
 public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapter.ViewHolder> {
     ArrayList<Friend> friends;
+    Context context;
     public MainActivityAdapter(ArrayList<Friend> friends){this.friends = friends;}
     @Override
     public MainActivityAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View friendListView = inflater.inflate(R.layout.item_view_friend, parent, false);
+        return new ViewHolder(friendListView);
     }
 
     @Override
     public void onBindViewHolder(MainActivityAdapter.ViewHolder holder, int position) {
-
+        Friend friend = friends.get(position);
+        String name = friend.getName();
+        holder.tvName.setText(name);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return friends.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView tvName;
+        ImageView ivPicture;
         public ViewHolder(View itemView){
             super(itemView);
-
-
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
+            ivPicture =  (ImageView) itemView.findViewById(R.id.ivPicture);
+            itemView.setOnClickListener(this);
         }// constructor
 
         @Override
         public void onClick(View view) {
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != android.support.v7.widget.RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Friend friend = friends.get(position);
+                // create intent for the new activity
+                // Intent intent = new Intent(context, OptionsActivity.class);
+                // serialize the country using parceler, use its short name as a key
+                // intent.putExtra("country", Parcels.wrap(country));
+                // show the activity
+                // context.startActivity(intent);
+            }
 
         }
     }
