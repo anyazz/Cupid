4833c3d5305e27e264862783b5e912c5fbbd9168
Brucegatete <bgatete@college.harvard.edu>
2017-11-11 15:17:31 -0500

    This is my version, whatever,rme

--- app/src/main/java/com/facebook/cupid/Match_Activity.java
+++ app/src/main/java/com/facebook/cupid/Match_Activity.java
@@ -8,23 +8,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class Match_Activity extends AppCompatActivity {
-    public Image imFirst;
-    public TextView tvFirst;
+    public ImageButton imFirst;
+    public TextView tvFirstName;
     public  TextView tvFirstBio;
-    public Image imSecond;
-    public TextView tvSecond;
+    public ImageButton imSecond;
+    public TextView tvSecondName;
     public  TextView tvSecondBio;
     public EditText etComment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_match_);
+        setContentView(R.layout.content_match_);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        imFirst = (ImageButton) findViewById(R.id.imFirst);
+        tvFirstName = (TextView) findViewById(R.id.tvFirstName);
+        tvFirstBio = (TextView) findViewById(R.id.tvFirstBio);
+        imSecond = (ImageButton) findViewById(R.id.imSecond);
+        tvSecondName = (TextView) findViewById(R.id.tvSecondName);
+        tvSecondBio = (TextView) findViewById(R.id.tvSecondBio);
+        etComment = (EditText) findViewById(R.id.etComment);
+
+
     }
 
 }
